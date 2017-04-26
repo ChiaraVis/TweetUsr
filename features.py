7eae14fe818f8c285d3cdc4e0676cd400171f2ee
Nikola Ljubešić <nljubesi@gmail.com>
2017-04-26 10:01:37 +0000

    Move from tweepy to JSON objects

--- features.py
+++ features.py
@@ -3,6 +3,7 @@
 import cPickle as pickle
 import numpy as np
 import re
+from dateutil.parser import parse
 from datetime import datetime
 import os
 import sys
@@ -23,74 +24,82 @@ def percentage(statuses,function):
 def user_meta(statuses,function):
   return function(statuses)
 
+emoji_pattern=re.compile(u'('
+    u'\ud83c[\udf00-\udfff]|'
+    u'\ud83d[\udc00-\ude4f\ude80-\udeff]|'
+    u'[\u2600-\u26FF\u2700-\u27BF])+', 
+    re.UNICODE)
+
+"""
 emoji_pattern=re.compile("["
         u"\U0001F600-\U0001F64F"  # emoticons
         u"\U0001F300-\U0001F5FF"  # symbols & pictographs
         u"\U0001F680-\U0001F6FF"  # transport & map symbols
         u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
         "]",re.UNICODE)
+"""
 
 def extract_features(statuses):
   x=[]
   ### tweet text features ###
   # mmv of tweet length
-  x.extend(mean_median_variance(statuses,lambda x:len(x.text)))
+  x.extend(mean_median_variance(statuses,lambda x:len(x['text'])))
   # URL
-  x.append(percentage(statuses,lambda x:'http' in x.text))
+  x.append(percentage(statuses,lambda x:'http' in x['text']))
   # mention
-  x.append(percentage(statuses,lambda x:'@' in x.text))
+  x.append(percentage(statuses,lambda x:'@' in x['text']))
   # hash
-  x.append(percentage(statuses,lambda x:'#' in x.text))
+  x.append(percentage(statuses,lambda x:'#' in x['text']))
   # question mark
-  x.append(percentage(statuses,lambda x:'?' in x.text))
+  x.append(percentage(statuses,lambda x:'?' in x['text']))
   # ellipsis
-  x.append(percentage(statuses,lambda x:'...' in x.text or u'…' in x.text))
+  x.append(percentage(statuses,lambda x:'...' in x['text'] or u'…' in x['text']))
   # comma
-  x.append(percentage(statuses,lambda x:',' in x.text))
+  x.append(percentage(statuses,lambda x:',' in x['text']))
   # exclamation mark
-  x.append(percentage(statuses,lambda x:'!' in x.text))
+  x.append(percentage(statuses,lambda x:'!' in x['text']))
   # emoji
-  x.append(percentage(statuses,lambda x:emoji_pattern.search(x.text)!=None))
+  x.append(percentage(statuses,lambda x:emoji_pattern.search(x['text'])!=None))
   ### tweet meta features ###
   # replies
-  x.append(percentage(statuses,lambda x:x.in_reply_to_user_id!=None))
+  x.append(percentage(statuses,lambda x:x['in_reply_to_user_id']!=None))
   # retweet count
-  x.extend(mean_median_variance(statuses,lambda x:x.retweet_count))
+  x.extend(mean_median_variance(statuses,lambda x:x['retweet_count']))
   # retweeted
-  x.append(percentage(statuses,lambda x:x.retweet_count>0))
+  x.append(percentage(statuses,lambda x:x['retweet_count']>0))
   # favorite count
-  x.extend(mean_median_variance(statuses,lambda x:x.favorite_count))
+  x.extend(mean_median_variance(statuses,lambda x:x['favorite_count']))
   # favorited
-  x.append(percentage(statuses,lambda x:x.favorite_count>0))
+  x.append(percentage(statuses,lambda x:x['favorite_count']>0))
   # quote
-  x.append(percentage(statuses,lambda x:x.is_quote_status==True))
+  x.append(percentage(statuses,lambda x:x['is_quote_status']==True))
   # mmv of hour
-  x.extend(mean_median_variance(statuses,lambda x:x.created_at.hour))
+  x.extend(mean_median_variance(statuses,lambda x:parse(x['created_at']).hour))
   # mmv of day
-  x.extend(mean_median_variance(statuses,lambda x:x.created_at.weekday()))
+  x.extend(mean_median_variance(statuses,lambda x:parse(x['created_at']).weekday()))
   # during working hours
-  x.append(percentage(statuses,lambda x:x.created_at.weekday()<6 and x.created_at.hour>=7 and x.created_at.hour<=18))
+  x.append(percentage(statuses,lambda x:parse(x['created_at']).weekday()<6 and parse(x['created_at']).hour>=7 and parse(x['created_at']).hour<=18))
   # on weekend
-  x.append(percentage(statuses,lambda x:x.created_at.weekday()>5))
+  x.append(percentage(statuses,lambda x:parse(x['created_at']).weekday()>5))
   # source twitter web client
-  x.append(percentage(statuses,lambda x:x.source==u'Twitter Web Client'))
+  x.append(percentage(statuses,lambda x:u'Twitter Web Client' in x['source']))
   # truncated
-  x.append(percentage(statuses,lambda x:x.truncated==True))
+  x.append(percentage(statuses,lambda x:x['truncated']==True))
   ### user meta features ###
   # number of tweets
-  x.append(user_meta(statuses,lambda x:x[-1].user.statuses_count))
+  x.append(user_meta(statuses,lambda x:x[-1]['user']['statuses_count']))
   # number of tweets per day
-  x.append(user_meta(statuses,lambda x:float(x[-1].user.statuses_count)/(datetime.now()-x[-1].user.created_at).days))
+  x.append(user_meta(statuses,lambda x:float(x[-1]['user']['statuses_count'])/(datetime.now()-parse(x[-1]['user']['created_at']).replace(tzinfo=None)).days))
   # followers count
-  x.append(user_meta(statuses,lambda x:x[-1].user.followers_count))
+  x.append(user_meta(statuses,lambda x:x[-1]['user']['followers_count']))
   # friends count
-  x.append(user_meta(statuses,lambda x:x[-1].user.friends_count))
+  x.append(user_meta(statuses,lambda x:x[-1]['user']['friends_count']))
   # followers friends ratio
-  x.append(user_meta(statuses,lambda x:float(x[-1].user.followers_count+0.1)/(x[-1].user.friends_count+0.1)))
+  x.append(user_meta(statuses,lambda x:float(x[-1]['user']['followers_count']+0.1)/(x[-1]['user']['friends_count']+0.1)))
   # listed_count
-  x.append(user_meta(statuses,lambda x:x[-1].user.listed_count))
+  x.append(user_meta(statuses,lambda x:x[-1]['user']['listed_count']))
   # favourites_count
-  x.append(user_meta(statuses,lambda x:x[-1].user.favourites_count))
+  x.append(user_meta(statuses,lambda x:x[-1]['user']['favourites_count']))
   return x
 
 if __name__=='__main__':
@@ -113,7 +122,7 @@ if __name__=='__main__':
     if len(files)<100:
       continue
     for file in files:
-      statuses.append(pickle.load(open('/home/nikola/tools/tweetpub/statuses/'+user+'/'+file)))
+      statuses.append(json.load(open('/home/nikola/tools/tweetpub/statuses/'+user+'/'+file)))
     print 'loaded',
     ### text for bow ###
     X.append(extract_features(statuses))
@@ -122,7 +131,6 @@ if __name__=='__main__':
     print 'extracted',
     print Y1[-1],Y2[-1],user,user_num+1,'/',len(users)
     sys.stdout.flush()
-
   pickle.dump(X,open('X.pickle','w'),1)
   pickle.dump(Y1,open('Y1.pickle','w'),1)
   pickle.dump(Y2,open('Y2.pickle','w'),1)
--- predict.py
+++ predict.py
@@ -1,8 +1,8 @@
 from features import extract_features
 import sys
 import os
-import cPickle as pickle
-statuses=[pickle.load(open(os.path.join(sys.argv[1],e))) for e in os.listdir(sys.argv[1])]
+import json
+statuses=[json.load(open(os.path.join(sys.argv[1],e))) for e in os.listdir(sys.argv[1])]
 from sklearn.externals import joblib
 clf = joblib.load('private_corporate.classifier') 
 features=extract_features(statuses)
